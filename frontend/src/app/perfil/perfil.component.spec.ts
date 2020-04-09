be97996d361669a034a7322f4453e9941a7479dc
JhonnyLosa <losajhonny@gmail.com>
2020-04-09 15:50:57 -0600

    pruebas unitarias componente perfil

--- frontend/src/app/perfil/perfil.component.spec.ts
+++ frontend/src/app/perfil/perfil.component.spec.ts
@@ -1,6 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
 import { PerfilComponent } from './perfil.component';
+import { RestService } from "../rest.service";
+import { HttpClientModule } from "@angular/common/http";
 
 describe('PerfilComponent', () => {
   let component: PerfilComponent;
@@ -8,7 +10,9 @@ describe('PerfilComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PerfilComponent ]
+      declarations: [ PerfilComponent ],
+      providers: [ RestService ],
+      imports: [ HttpClientModule ]
     })
     .compileComponents();
   }));
@@ -22,4 +26,93 @@ describe('PerfilComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("deberia crear un objeto perfil", () => {
+    let perfil = component.crearPerfil("", "", -1, "", 0, "", "");
+    expect(perfil.nombre).toBeDefined();
+    expect(perfil.apellido).toBeDefined();
+    expect(perfil.dpi).toBeDefined();
+    expect(perfil.nocuenta).toBeDefined();
+    expect(perfil.saldo).toBeDefined();
+    expect(perfil.email).toBeDefined();
+    expect(perfil.password).toBeDefined();
+  });
+
+  it("deberia de estar inicializado nombre", () => {
+    expect(component.nombre).toBeDefined();
+  });
+
+  it("deberia de estar inicializado apellido", () => {
+    expect(component.apellido).toBeDefined();
+  });
+
+  it("deberia de estar inicializado dpi", () => {
+    expect(component.dpi).toBeDefined();
+  });
+
+  it("deberia de estar inicializado nocuenta", () => {
+    expect(component.nocuenta).toBeDefined();
+  });
+
+  it("deberia de estar inicializado saldo", () => {
+    expect(component.saldo).toBeDefined();
+  });
+
+  it("deberia de estar inicializado email", () => {
+    expect(component.email).toBeDefined();
+  });
+
+  it("deberia de estar inicializado password", () => {
+    expect(component.password).toBeDefined();
+  });
+
+  it("deberia recibir los campos \"estado\", \"mensaje\" y \"result\"",
+   inject([RestService], async (rest: RestService) => {
+     let result;
+
+     result = await rest.PostRequest("perfil", {}).toPromise();
+     expect(result.estado).toBeDefined();
+     expect(result.mensaje).toBeDefined();
+     expect(result.result).toBeDefined();
+   })
+  );
+
+  it("deberia recibir el result con los valores del usuario",
+   inject([RestService], async (rest: RestService) => {
+     let result;
+
+     // prueba para la peticion correcta
+     result = await rest.PostRequest("perfil", {}).toPromise();
+     expect(result.estado).toBeDefined();
+     expect(result.mensaje).toBeDefined();
+     expect(result.result).toBeDefined();
+
+     expect(result.estado).toBeTruthy();
+   })
+  );
+
+  it("deberia recibir el result con los valores por defecto",
+   inject([RestService], async (rest: RestService) => {
+     let result;
+
+     // prueba para la peticion incorrecta
+     result = await rest.PostRequest("perfil", {}).toPromise();
+     expect(result.estado).toBeDefined();
+     expect(result.mensaje).toBeDefined();
+     expect(result.result).toBeDefined();
+
+     // como aun no funciona el servidor coloco "!" (aun no hay funcionalidad)
+     /******************************************************/
+     expect(!result.estado).toBeFalsy();
+
+     // validar que los valores tengan un valor por defecto
+     expect(result.result.nombre).toEqual("")
+     expect(result.result.apellido).toEqual("");
+     expect(result.result.dpi).toEqual(-1);
+     expect(result.result.nocuenta).toEqual("");
+     expect(result.result.saldo).toEqual(0);
+     expect(result.result.email).toEqual("");
+     expect(result.result.password).toEqual("");
+   })
+  );
 });
--- frontend/src/app/perfil/perfil.component.ts
+++ frontend/src/app/perfil/perfil.component.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  nombre: string;
+  apellido: string;
+  dpi: number;
+  nocuenta: string;
+  saldo: number;
+  email: string;
+  password: string;
+
+  constructor() {
+    this.nombre = "";
+    this.apellido = "";
+    this.dpi = -1;
+    this.nocuenta = "";
+    this.saldo = 0;
+    this.email = "";
+    this.password = "";
+  }
 
   ngOnInit() {
+  }
+
+  crearPerfil(nombre: string, apellido: string, dpi: number, nocuenta: string, saldo: number, email: string, password: string) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.dpi = dpi;
+    this.nocuenta = nocuenta;
+    this.saldo = saldo;
+    this.email = email;
+    this.password = password;
+
+    return {
+      nombre: nombre,
+      apellido: apellido,
+      dpi: dpi,
+      nocuenta: nocuenta,
+      saldo: saldo,
+      email: email,
+      password: password
+    };
   }
 
 }
