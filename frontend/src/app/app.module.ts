85b8f20a81b9d818ba131902b04289e438ca7e0b
201213225 <kandor95@gmail.com>
2020-04-09 21:24:45 -0600

    creadion componente

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { SignupComponent } from './signup/signup.component';
 import { RestService } from './rest.service';
 import { CheckBalanceComponent } from './check-balance/check-balance.component';
 import { PerfilComponent } from './perfil/perfil.component';
+import { MoneyTransferComponent } from './money-transfer/money-transfer.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { PerfilComponent } from './perfil/perfil.component';
     LoginComponent,
     SignupComponent,
     CheckBalanceComponent,
-    PerfilComponent
+    PerfilComponent,
+    MoneyTransferComponent
   ],
   imports: [
     BrowserModule,
