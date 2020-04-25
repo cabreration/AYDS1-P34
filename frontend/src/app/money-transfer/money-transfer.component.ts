c1107230644033ee8c7f9e70e2b885e52b1fffc2
josecbm <jcbautista95@gmail.com>
2020-04-25 15:45:55 -0600

    money transfer added

--- frontend/src/app/money-transfer/money-transfer.component.ts
+++ frontend/src/app/money-transfer/money-transfer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RestService } from '../rest.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-money-transfer',
   templateUrl: './money-transfer.component.html',
@@ -11,22 +12,48 @@ export class MoneyTransferComponent implements OnInit {
   * CUANDO LA TRANSFERENCIA MONETARIA SEA EXITOSA EL MONTO ENVIADO SE DEBE RESTAR DIRECTAMENTE
   * DE LA VARIABLE DE SESION, ASI EVITAMOS HACER PETICIONES INNECESARIAS
   */
-  constructor() { }
+  noCuentaActual = 122;
+  alerta = '';
+  jsonUsrActual;
+  cantidad = 0;
+  cuentaDepositar = 0;
+  constructor(private rest: RestService, private router: Router) { }
 
   ngOnInit() {
+    this.jsonUsrActual = JSON.parse(sessionStorage.getItem('user'));
   }
 
-  compararMontos(montoATransferir) {
+  async compararMontos(montoATransferir) {
     let user = JSON.parse(sessionStorage.getItem('user'));
     if (user == null || user == undefined) {
       return false;
     }
     if (montoATransferir > user.balance) {
+      console.log('monto sobrepasa fondos en cuenta');
+      this.alerta = 'Fondos insuficientes';
+      setTimeout(() => this.alerta = '', 2000);
       return false;
     }
     else {
+      let obj = this.createTransferObject(user.balance , this.cuentaDepositar,this.cantidad);
+      let registerTransfer = await this.rest.PostRequest('money-transfer', obj).toPromise();
+      if(registerTransfer){
+        console.log("Transferencia realizada exitosamente");
+        this.alerta = 'Transferencia realizada exitosamente';
+      setTimeout(() => this.alerta = '', 2000);
+      return true;
+      }
       return true;
     }
+    /**
+     *  let origen = req.body.origen;
+  let destino = req.body.destino;
+  let cantidad = req.body.cantidad;
+  let fecha = new Date();
+     */
+  }
+  createTransferObject(origen: number, destino: number, cantidad: number): any {
+    return { origen: origen, destino: destino , cantidad:cantidad};
   }
 
 }
