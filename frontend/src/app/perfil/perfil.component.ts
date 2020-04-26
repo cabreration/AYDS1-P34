af323e8ebeec83534d9eaa8f416daf13b7095c46
JhonnyLosa <losajhonny@gmail.com>
2020-04-26 00:16:13 -0600

    terminando funcionalidad
    Se termino la vista y su funcionalidad queda pendiente mejorar las pruebas unitarias

--- frontend/src/app/perfil/perfil.component.ts
+++ frontend/src/app/perfil/perfil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from '../rest.service';
 
 @Component({
   selector: 'app-perfil',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  id: string;
+  usuario: any;
   nombre: string;
   apellido: string;
   dpi: number;
@@ -19,8 +20,10 @@ export class PerfilComponent implements OnInit {
   passwordNuevo: string;
   passwordConfirmar: string;
 
-  constructor() {
-    this.id = "";
+  mensaje: string;
+  alerta: string;
+
+  constructor(private rest: RestService) {
     this.nombre = "";
     this.apellido = "";
     this.dpi = -1;
@@ -30,9 +33,52 @@ export class PerfilComponent implements OnInit {
     this.password = "";
     this.passwordNuevo = "";
     this.passwordConfirmar = "";
+    this.mensaje = "";
+    this.alerta = "";
+
+    this.cargarUsuario();
   }
 
   ngOnInit() {
+  }
+
+  cargarUsuario() {
+    let user = sessionStorage.getItem("user");
+    let usuario = JSON.parse(user);
+    //console.log(usuario);
+
+    this.usuario = usuario;
+
+    this.nombre = usuario.name;
+    this.apellido = usuario.lastName;
+    this.dpi = usuario.dpi;
+    this.nocuenta = usuario.account;
+    this.saldo = usuario.balance;
+    this.email = usuario.email;
+  }
+
+  crearPerfil(nombre: string, apellido: string, dpi: number, nocuenta: string, saldo: number, email: string, password: string) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.dpi = dpi;
+    this.nocuenta = nocuenta;
+    this.saldo = saldo;
+    this.email = email;
+    this.password = password;
+
+    return {
+      nombre: nombre,
+      apellido: apellido,
+      dpi: dpi,
+      nocuenta: nocuenta,
+      saldo: saldo,
+      email: email,
+      password: password
+    };
+  }
+
+  changeEmail(email) {
+    this.email = email;
   }
 
   changeNombre(nombre) {
@@ -59,28 +105,69 @@ export class PerfilComponent implements OnInit {
     this.passwordConfirmar = password;
   }
 
-  eventActualizar() {
-    /*enviar al servidor*/
+  checkFields() {
+    if(this.password === "" && (this.passwordNuevo !== "" || this.passwordConfirmar !== "")) {
+      this.alerta = "Debe de ingresar su password";
+      setTimeout(() => this.alerta = '', 2000);
+      return false;
+    }
+
+    if(this.password !== "" && (this.passwordNuevo === "" && this.passwordConfirmar === "")) {
+      this.alerta = "Debe de ingresar su nuevo password";
+      setTimeout(() => this.alerta = "", 2000);
+      return false;
+    }
+
+    if(this.passwordNuevo === "" && this.passwordConfirmar === "") {
+      //no modificar password
+      return true;
+    }
+
+    if(this.passwordNuevo !== this.passwordConfirmar) {
+      this.alerta = "Su password no coincide";
+      setTimeout(() => this.alerta = "", 2000);
+      return false;
+    }
+    return true;
   }
 
-  crearPerfil(nombre: string, apellido: string, dpi: number, nocuenta: string, saldo: number, email: string, password: string) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.dpi = dpi;
-    this.nocuenta = nocuenta;
-    this.saldo = saldo;
-    this.email = email;
-    this.password = password;
+  async eventActualizar() {
+    /*checkear campos*/
+    if(!this.checkFields()) return;
 
-    return {
-      nombre: nombre,
-      apellido: apellido,
-      dpi: dpi,
-      nocuenta: nocuenta,
-      saldo: saldo,
-      email: email,
-      password: password
-    };
+    /*construir info*/
+    const info = {
+      account: this.nocuenta,
+      name: this.nombre,
+      lastName: this.apellido,
+      dpi: this.dpi,
+      balance: this.saldo,
+      email: this.email,
+      password: this.password,
+      passwordNew: this.passwordNuevo,
+      passwordSesion: this.usuario.password
+    }
+
+    /* realizar peticion */
+    //console.log(this.usuario);
+    let res = await this.rest.PostRequest("perfil", info).toPromise();
+
+    if(res.estado) {
+
+      if(res.result !== null) {
+        // restaurar sesion
+        sessionStorage.setItem('user', JSON.stringify(res.result));
+
+        // recuperar los demas datos
+        this.cargarUsuario();
+      }
+
+      this.mensaje = res.mensaje;
+      setTimeout(() => this.mensaje = "", 2000);
+    }
+    else {
+      this.alerta = res.mensaje;
+      setTimeout(() => this.alerta = "", 2000);
+    }
   }
-
 }
