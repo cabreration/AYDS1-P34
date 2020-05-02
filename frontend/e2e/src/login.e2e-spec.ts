764e350aa2c37443df0e9f81abb4080094204088
Jav <j.a.cabrerapuente@gmail.com>
2020-05-02 07:50:37 -0600

    sleeps taken off

--- frontend/e2e/src/login.e2e-spec.ts
+++ frontend/e2e/src/login.e2e-spec.ts
@@ -11,31 +11,31 @@ describe('vista de login', () => {
     it('deberia navegar a la pagina de registro', () => {
         page.navigateTo();
 
-        browser.driver.sleep(3000);
+        //browser.driver.sleep(3000);
         page.getSignupButton().click();
         expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/signup');
-        browser.driver.sleep(3000);
+        //browser.driver.sleep(3000);
     });
 
     it('deberia navegar a la pagina de perfil al iniciar sesion', () => {
         page.navigateTo();
 
-        browser.driver.sleep(1500);
+        //browser.driver.sleep(1500);
         page.getAccountField().sendKeys('test');
-        browser.driver.sleep(1500);
+        //browser.driver.sleep(1500);
         page.getPasswordField().sendKeys('test');
-        browser.driver.sleep(1500);
+        //browser.driver.sleep(1500);
         page.getLoginButton().click();
         expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/perfil');
-        browser.driver.sleep(3000);
+        //browser.driver.sleep(3000);
     });
 
     it('deberia mostrar una advertencia si los campos no son llenados', ()=> {
         page.navigateTo();
 
-        browser.driver.sleep(3000);
+        //browser.driver.sleep(3000);
         expect(page.getAlert().isPresent()).toBeFalsy();
         page.getLoginButton().click();
-        browser.driver.sleep(3000);
+        //browser.driver.sleep(3000);
     });
 });
